73b1993d5dc6f5722ff11a7f4c18fa4610f519e6
Shunwei Min <shunwei.min.19@ucl.ac.uk>
2020-01-03 14:21:49 +0000

    add test_class_time

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -6,3 +6,12 @@ def test_given_input():
     result = times.overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_class_time():
+    large = times.time_range("2019-10-31 10:00:00" , "2019-10-31 13:00:00")
+    short = times.time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600)
+    # two breaks, therefore 3 blocks
+    result = times.overlap_time(large, short) 
+    assert result == short
+    # smaller time encompasses the bigger one (small time in a big time)
+    # expected results is the shorter time range
